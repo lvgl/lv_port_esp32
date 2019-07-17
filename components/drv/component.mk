9422a1c2499be163ff4213a8884bb7378d6d5519
Carlos <carlos.santiago.diaz@gmail.com>
2019-07-16 23:53:05 -0500

    Add option on drv\component.mk to enable/disable touch input support

--- components/drv/component.mk
+++ components/drv/component.mk
@@ -3,6 +3,9 @@
 CFLAGS += -DLV_CONF_INCLUDE_SIMPLE
 CFLAGS += -DILI9341_BCKL_ACTIVE_LVL=0
 
+# Set to 1 if your display have touch support, otherwise set it to 0.
+CFLAGS += -DTOUCH_SUPPORT=1
+
 COMPONENT_SRCDIRS := . 
 	
 COMPONENT_ADD_INCLUDEDIRS := $(COMPONENT_SRCDIRS) .. ../
