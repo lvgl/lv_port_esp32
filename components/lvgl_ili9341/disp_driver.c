4a242f8ea29ee7283ef3a15bfb437f839618763b
C47D <carlos.santiago.diaz@gmail.com>
2019-12-28 12:48:30 -0600

    [disp_driver] Add disp driver abstraction layer

---
+++ components/lvgl_ili9341/disp_driver.c
@@ -0,0 +1,26 @@
+/**
+ * @file disp_driver.c
+ */
+
+#include "disp_driver.h"
+
+#include "ili9341.h"
+#include "ili9488.h"
+
+void disp_driver_init(void)
+{
+#if CONFIG_LVGL_TFT_DISPLAY_CONTROLLER == 0
+    ili9341_init();
+#elif CONFIG_LVGL_TFT_DISPLAY_CONTROLLER == 1
+    ili9488_init();
+#endif
+}
+
+void disp_driver_flush(lv_disp_drv_t * drv, const lv_area_t * area, lv_color_t * color_map)
+{
+#if CONFIG_LVGL_TFT_DISPLAY_CONTROLLER == 0
+    ili9341_flush(drv, area, color_map);
+#elif CONFIG_LVGL_TFT_DISPLAY_CONTROLLER == 1
+    ili9488_flush(drv, area, color_map);
+#endif
+}
