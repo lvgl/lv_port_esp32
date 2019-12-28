96d5e4031450b31cfeb142e9aa7b80b8b29ac8c0
C47D <carlos.santiago.diaz@gmail.com>
2019-12-28 11:07:31 -0600

    [ILI9488] Add ILI9488 basic functions

---
+++ components/lvgl_ili9341/ili9488.h
@@ -0,0 +1,58 @@
+/**
+ * @file ili9488.h
+ */
+
+#ifndef ILI9844_H
+#define ILI9844_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include <stdbool.h>
+
+#include "lvgl/lvgl.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+#define DISP_BUF_SIZE (LV_HOR_RES_MAX * 40)
+#define ILI9488_DC   CONFIG_LVGL_DISP_PIN_DC
+#define ILI9488_RST  CONFIG_LVGL_DISP_PIN_RST
+#define ILI9488_BCKL CONFIG_LVGL_DISP_PIN_BCKL
+
+#define ILI9488_ENABLE_BACKLIGHT_CONTROL CONFIG_LVGL_ENABLE_BACKLIGHT_CONTROL
+
+#if CONFIG_LVGL_BACKLIGHT_ACTIVE_LVL
+  #define ILI9488_BCKL_ACTIVE_LVL 1
+#else
+  #define ILI9488_BCKL_ACTIVE_LVL 0
+#endif
+
+// if text/images are backwards, try setting this to 1
+#define ILI9488_INVERT_DISPLAY CONFIG_LVGL_INVERT_DISPLAY
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+void ili9488_init(void);
+void ili9488_flush(lv_disp_drv_t * drv, const lv_area_t * area, lv_color_t * color_map);
+void ili9488_enable_backlight(bool backlight);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*ILI9488_H*/
