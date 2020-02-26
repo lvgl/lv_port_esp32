6060f6cdc8ad0f80df5d1ff91e2c1bcba562e8a8
Tony Stuart <anthonyfstuart@gmail.com>
2020-02-25 21:52:12 -0800

    Split out display SPI configuration functions for more flexible configuration

--- components/lvgl_esp32_drivers/lvgl_tft/disp_spi.h
+++ components/lvgl_esp32_drivers/lvgl_tft/disp_spi.h
@@ -15,6 +15,7 @@ extern "C" {
  *********************/
 #include <stdint.h>
 #include <stdbool.h>
+#include <driver/spi_master.h>
 
 /*********************
  *      DEFINES
@@ -33,6 +34,8 @@ extern "C" {
  * GLOBAL PROTOTYPES
  **********************/
 void disp_spi_init(void);
+void disp_spi_add_device(spi_host_device_t host);
+void disp_spi_add_device_config(spi_host_device_t host, spi_device_interface_config_t *devcfg);
 void disp_spi_send_data(uint8_t * data, uint16_t length);
 void disp_spi_send_colors(uint8_t * data, uint16_t length);
 bool disp_spi_is_busy(void);
