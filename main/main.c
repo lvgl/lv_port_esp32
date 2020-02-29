8d3ed5370795287854751b0024b1243fe51b28de
C47D <carlos.santiago.diaz@gmail.com>
2020-02-29 12:05:23 -0600

    Update main to use the new touch driver layer

--- main/main.c
+++ main/main.c
@@ -11,22 +11,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
-#include "driver/gpio.h"
-#include "lvgl/lvgl.h"
-#include "lv_examples/lv_apps/demo/demo.h"
 #include "esp_freertos_hooks.h"
 
+#include "esp_system.h"
+#include "driver/gpio.h"
+
+/* Littlevgl specific */
+#include "lvgl/lvgl.h"
+#include "lv_examples/lv_apps/demo/demo.h"
 
 #include "disp_spi.h"
 #include "disp_driver.h"
-
-#include "tp_spi.h"
-#include "xpt2046.h"
-#include "tp_i2c.h"
-#include "ft6x36.h"
+#include "touch_driver.h"
 
 static void IRAM_ATTR lv_tick_task(void);
 
@@ -46,12 +45,10 @@ void app_main() {
     disp_spi_init();
     disp_driver_init();
 
-#if CONFIG_LVGL_TOUCH_CONTROLLER == 1
-    tp_spi_init();
-    xpt2046_init();
-#elif CONFIG_LVGL_TOUCH_CONTROLLER == 2
-    ft6x06_init(FT6236_I2C_SLAVE_ADDR);
+#if CONFIG_LVGL_TOUCH_CONTROLLER != TOUCH_CONTROLLER_NONE
+    touch_init();
 #endif
+
 #endif
 
     static lv_color_t buf1[DISP_BUF_SIZE];
@@ -65,16 +62,10 @@ void app_main() {
     disp_drv.buffer = &disp_buf;
     lv_disp_drv_register(&disp_drv);
 
-#if CONFIG_LVGL_TOUCH_CONTROLLER == 1
+#if CONFIG_LVGL_TOUCH_CONTROLLER != TOUCH_CONTROLLER_NONE
     lv_indev_drv_t indev_drv;
     lv_indev_drv_init(&indev_drv);
-    indev_drv.read_cb = xpt2046_read;
-    indev_drv.type = LV_INDEV_TYPE_POINTER;
-    lv_indev_drv_register(&indev_drv);
-#elif CONFIG_LVGL_TOUCH_CONTROLLER == 2
-    lv_indev_drv_t indev_drv;
-    lv_indev_drv_init(&indev_drv);
-    indev_drv.read_cb = ft6x36_read;
+    indev_drv.read_cb = touch_read;
     indev_drv.type = LV_INDEV_TYPE_POINTER;
     lv_indev_drv_register(&indev_drv);
 #endif
