19b9f25e4954ec5ea802f1a2387ed867836f7128
C47D <carlos.santiago.diaz@gmail.com>
2020-02-29 00:04:11 -0600

    Add some documentation on main.c

--- main/main.c
+++ main/main.c
@@ -1,11 +1,13 @@
-/* SPI Master example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+/* LVGL Example project
+ * 
+ * Basic project to test LVGL on ESP32 based projects.
+ *
+ * This example code is in the Public Domain (or CC0 licensed, at your option.)
+ *
+ * Unless required by applicable law or agreed to in writing, this
+ * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -29,52 +31,18 @@
 static void IRAM_ATTR lv_tick_task(void);
 
 #if CONFIG_LVGL_TOUCH_CONTROLLER == 1 && TP_SPI_MOSI == DISP_SPI_MOSI && TP_SPI_CLK == DISP_SPI_CLK
-static void configure_shared_spi_bus()
-{
-    spi_bus_config_t buscfg = {
-        .miso_io_num = TP_SPI_MISO,
-        .mosi_io_num = TP_SPI_MOSI,
-        .sclk_io_num = TP_SPI_CLK,
-        .quadwp_io_num = -1,
-        .quadhd_io_num = -1,
-        .max_transfer_sz = DISP_BUF_SIZE * 2,
-    };
-
-    esp_err_t ret = spi_bus_initialize(HSPI_HOST, &buscfg, 1);
-    assert(ret == ESP_OK);
-
-    spi_device_interface_config_t ili9341_config = {
-        .clock_speed_hz = 20 * 1000 * 1000,
-        .mode = 0,
-        .spics_io_num = DISP_SPI_CS,
-        .queue_size = 1,
-        .pre_cb = NULL,
-        .post_cb = NULL,
-    };
-
-    disp_spi_add_device_config(HSPI_HOST, &ili9341_config);
-    disp_driver_init();
-
-    spi_device_interface_config_t xpt2046_config = {
-        .clock_speed_hz = 2 * 1000 * 1000,
-        .mode = 0,
-        .spics_io_num = TP_SPI_CS,
-        .queue_size = 1,
-        .pre_cb = NULL,
-        .post_cb = NULL,
-    };
-
-    tp_spi_add_device_config(HSPI_HOST, &xpt2046_config);
-    xpt2046_init();
-}
+/* Example function that configure two spi devices (tft and touch controllers) into the same spi bus */
+static void configure_shared_spi_bus(void);
 #endif
 
 void app_main() {
     lv_init();
 
+    /* Configure the SPI bus and SPI devices to handle the tft and touch controllers */
 #if CONFIG_LVGL_TOUCH_CONTROLLER == 1 && TP_SPI_MOSI == DISP_SPI_MOSI && TP_SPI_CLK == DISP_SPI_CLK
     configure_shared_spi_bus();
 #else
+    /* Otherwise configure the SPI bus and devices separately */
     disp_spi_init();
     disp_driver_init();
 
@@ -124,3 +92,44 @@ void app_main() {
 static void IRAM_ATTR lv_tick_task(void) {
     lv_tick_inc(portTICK_RATE_MS);
 }
+
+#if CONFIG_LVGL_TOUCH_CONTROLLER == 1 && TP_SPI_MOSI == DISP_SPI_MOSI && TP_SPI_CLK == DISP_SPI_CLK
+static void configure_shared_spi_bus(void)
+{
+    spi_bus_config_t buscfg = {
+        .miso_io_num = TP_SPI_MISO,
+        .mosi_io_num = TP_SPI_MOSI,
+        .sclk_io_num = TP_SPI_CLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = DISP_BUF_SIZE * 2,
+    };
+
+    esp_err_t ret = spi_bus_initialize(HSPI_HOST, &buscfg, 1);
+    assert(ret == ESP_OK);
+
+    spi_device_interface_config_t ili9341_config = {
+        .clock_speed_hz = 20 * 1000 * 1000,
+        .mode = 0,
+        .spics_io_num = DISP_SPI_CS,
+        .queue_size = 1,
+        .pre_cb = NULL,
+        .post_cb = NULL,
+    };
+
+    disp_spi_add_device_config(HSPI_HOST, &ili9341_config);
+    disp_driver_init();
+
+    spi_device_interface_config_t xpt2046_config = {
+        .clock_speed_hz = 2 * 1000 * 1000,
+        .mode = 0,
+        .spics_io_num = TP_SPI_CS,
+        .queue_size = 1,
+        .pre_cb = NULL,
+        .post_cb = NULL,
+    };
+
+    tp_spi_add_device_config(HSPI_HOST, &xpt2046_config);
+    xpt2046_init();
+}
+#endif
