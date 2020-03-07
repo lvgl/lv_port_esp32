f244815e456251d2a0e696cfd9ff8010f772b305
C47D <carlos.santiago.diaz@gmail.com>
2020-03-07 14:43:41 -0600

    ST7789: Set MADCTL to 0x08
    With MADCTL set to 0x28 the display image was flipped.

--- components/lvgl_esp32_drivers/lvgl_tft/st7789.c
+++ components/lvgl_esp32_drivers/lvgl_tft/st7789.c
@@ -59,7 +59,7 @@ void st7789_init(void)
         {ST7789_IDSET, {0x11}, 1},
         {ST7789_VCMOFSET, {0x35, 0x3E}, 2},
         {ST7789_CABCCTRL, {0xBE}, 1},
-        {ST7789_MADCTL, {0x28}, 1},
+        {ST7789_MADCTL, {0x08}, 1}, // Set to 0x28 if your display is flipped
         {ST7789_COLMOD, {0x55}, 1},
         {ST7789_RGBCTRL, {0x00, 0x1B}, 2},
         {0xF2, {0x08}, 1},
