26e53b3a1275f8d634c64f15aba9574620c33cd9
C47D <carlos.santiago.diaz@gmail.com>
2020-05-03 19:53:00 -0500

    ili9341: Remove DISP_BUF_SIZE symbol definition

--- components/lvgl_esp32_drivers/lvgl_tft/ili9341.h
+++ components/lvgl_esp32_drivers/lvgl_tft/ili9341.h
@@ -16,11 +16,11 @@ extern "C" {
 #include <stdbool.h>
 
 #include "lvgl/lvgl.h"
+#include "../lvgl_driver.h"
 
 /*********************
  *      DEFINES
  *********************/
-#define DISP_BUF_SIZE (LV_HOR_RES_MAX * 40)
 #define ILI9341_DC   CONFIG_LVGL_DISP_PIN_DC
 #define ILI9341_RST  CONFIG_LVGL_DISP_PIN_RST
 #define ILI9341_BCKL CONFIG_LVGL_DISP_PIN_BCKL
