94b891a13f15c6d5a86cd2ba9ebe663db8929363
Wolfgang <24637325+seeul8er@users.noreply.github.com>
2020-05-05 21:54:14 +0200

    Fixed loop. Added log warning

--- components/lvgl_esp32_drivers/lvgl_tft/ili9488.c
+++ components/lvgl_esp32_drivers/lvgl_tft/ili9488.c
@@ -121,10 +121,11 @@ void ili9488_flush(lv_disp_drv_t * drv, const lv_area_t * area, lv_color_t * col
     uint32_t size = lv_area_get_width(area) * lv_area_get_height(area);
 
     lv_color16_t *buffer_16bit = (lv_color16_t *) color_map;
-    uint8_t *mybuf = (uint8_t *) heap_caps_malloc(3 * size * sizeof(uint8_t), MALLOC_CAP_DMA);
-    while (mybuf == NULL) {
+    uint8_t *mybuf;
+    do {
         mybuf = (uint8_t *) heap_caps_malloc(3 * size * sizeof(uint8_t), MALLOC_CAP_DMA);
-    }
+        if (mybuf == NULL)  ESP_LOGW(TAG, "Could not allocate enough DMA memory!");
+    } while (mybuf == NULL);
 
     uint32_t LD = 0;
     uint32_t j = 0;
