527094272cfd9bf519c373b4688d9399ae93104b
C47D <carlos.santiago.diaz@gmail.com>
2020-06-03 14:42:14 -0500

    disp driver: Generic cleanup

--- components/lvgl_esp32_drivers/lvgl_tft/disp_driver.c
+++ components/lvgl_esp32_drivers/lvgl_tft/disp_driver.c
@@ -66,7 +66,7 @@ void disp_driver_rounder(lv_disp_drv_t * disp_drv, lv_area_t * area)
 #endif
 }
 
-void disp_driver_set_px(struct _disp_drv_t * disp_drv, uint8_t * buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
+void disp_driver_set_px(lv_disp_drv_t * disp_drv, uint8_t * buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
     lv_color_t color, lv_opa_t opa) 
 {
 #if defined CONFIG_LVGL_TFT_DISPLAY_CONTROLLER_SSD1306
--- components/lvgl_esp32_drivers/lvgl_tft/disp_driver.h
+++ components/lvgl_esp32_drivers/lvgl_tft/disp_driver.h
@@ -12,7 +12,6 @@ extern "C" {
 /*********************
  *      INCLUDES
  *********************/
-#include <stdbool.h>
 #include "lvgl/lvgl.h"
 
 #if defined CONFIG_LVGL_TFT_DISPLAY_CONTROLLER_ILI9341
@@ -48,10 +47,18 @@ extern "C" {
 /**********************
  * GLOBAL PROTOTYPES
  **********************/
+
+/* Initialize display */
 void disp_driver_init(void);
+
+/* Display flush callback */
 void disp_driver_flush(lv_disp_drv_t * drv, const lv_area_t * area, lv_color_t * color_map);
+
+/* Display rounder callback, used with monochrome dispays */
 void disp_driver_rounder(lv_disp_drv_t * disp_drv, lv_area_t * area);
-void disp_driver_set_px(struct _disp_drv_t * disp_drv, uint8_t * buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
+
+/* Display set_px callback, used with monochrome dispays */
+void disp_driver_set_px(lv_disp_drv_t * disp_drv, uint8_t * buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
     lv_color_t color, lv_opa_t opa);
 
 /**********************
