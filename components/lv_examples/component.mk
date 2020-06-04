ad9ab4a6b2070c0e923366345321a2ce885b10ad
C47D <carlos.santiago.diaz@gmail.com>
2020-06-04 10:35:42 -0500

    lv_examples: Add assets directory to sources

--- components/lv_examples/component.mk
+++ components/lv_examples/component.mk
@@ -10,6 +10,7 @@ COMPONENT_SRCDIRS := lv_examples           \
     lv_examples/src/demo_stress            \
     lv_examples/src/lv_demo_widgets        \
     lv_examples/src/lv_ex_style            \
-    lv_examples/src/lv_ex_widgets
+    lv_examples/src/lv_ex_widgets          \
+    lv_examples/assets
 
 COMPONENT_ADD_INCLUDEDIRS := $(COMPONENT_SRCDIRS) .
