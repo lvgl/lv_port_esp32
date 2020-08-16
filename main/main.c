d89070a2c6a2f94f6d5e38ae0a6c0a842925fd42
C47D <carlos.santiago.diaz@gmail.com>
2020-08-16 00:46:59 -0500

    add comments to main

--- main/main.c
+++ main/main.c
@@ -24,7 +24,9 @@
 #include "lvgl/lvgl.h"
 #include "lvgl_helpers.h"
 
+#ifndef CONFIG_LVGL_TFT_DISPLAY_MONOCHROME
 #include "lv_examples/src/lv_demo_widgets/lv_demo_widgets.h"
+#endif
 
 /*********************
  *      DEFINES
@@ -36,31 +38,26 @@
  *  STATIC PROTOTYPES
  **********************/
 static void lv_tick_task(void *arg);
-void guiTask(void *pvParameter);
-
+static void guiTask(void *pvParameter);
+static void create_demo_application(void);
 
 /**********************
  *   APPLICATION MAIN
  **********************/
 void app_main() {
     
-    //If you want to use a task to create the graphic, you NEED to create a Pinned task
-    //Otherwise there can be problem such as memory corruption and so on
+    /* If you want to use a task to create the graphic, you NEED to create a Pinned task
+     * Otherwise there can be problem such as memory corruption and so on.
+     * NOTE: When not using Wi-Fi nor Bluetooth you can pin the guiTask to core 0 */
     xTaskCreatePinnedToCore(guiTask, "gui", 4096*2, NULL, 0, NULL, 1);
 }
 
-static void lv_tick_task(void *arg) {
-    (void) arg;
-
-    lv_tick_inc(LV_TICK_PERIOD_MS);
-}
-
-//Creates a semaphore to handle concurrent call to lvgl stuff
-//If you wish to call *any* lvgl function from other threads/tasks
-//you should lock on the very same semaphore!
+/* Creates a semaphore to handle concurrent call to lvgl stuff
+ * If you wish to call *any* lvgl function from other threads/tasks
+ * you should lock on the very same semaphore! */
 SemaphoreHandle_t xGuiSemaphore;
 
-void guiTask(void *pvParameter) {
+static void guiTask(void *pvParameter) {
     
     (void) pvParameter;
     xGuiSemaphore = xSemaphoreCreateMutex();
@@ -70,13 +67,17 @@ void guiTask(void *pvParameter) {
     /* Initialize SPI or I2C bus used by the drivers */
     lvgl_driver_init();
 
+    /* Use double buffered when not working with monochrome displays */
     static lv_color_t buf1[DISP_BUF_SIZE];
 #ifndef CONFIG_LVGL_TFT_DISPLAY_MONOCHROME
     static lv_color_t buf2[DISP_BUF_SIZE];
 #endif
+
     static lv_disp_buf_t disp_buf;
 
     uint32_t size_in_px = DISP_BUF_SIZE;
+
+    /* Initialize the working buffer depending on the selected display */
 
 #if defined CONFIG_LVGL_TFT_DISPLAY_CONTROLLER_IL3820 
     /* Actual size in pixel, not bytes and use single buffer */
@@ -92,6 +93,9 @@ void guiTask(void *pvParameter) {
     lv_disp_drv_init(&disp_drv);
     disp_drv.flush_cb = disp_driver_flush;
 
+    /* When using a monochrome display we need to register the callbacks:
+     * - rounder_cb
+     * - set_px_cb */
 #ifdef CONFIG_LVGL_TFT_DISPLAY_MONOCHROME
     disp_drv.rounder_cb = disp_driver_rounder;
     disp_drv.set_px_cb = disp_driver_set_px;
@@ -100,6 +104,7 @@ void guiTask(void *pvParameter) {
     disp_drv.buffer = &disp_buf;
     lv_disp_drv_register(&disp_drv);
 
+    /* Register an input device when enabled on the menuconfig */
 #if CONFIG_LVGL_TOUCH_CONTROLLER != TOUCH_CONTROLLER_NONE
     lv_indev_drv_t indev_drv;
     lv_indev_drv_init(&indev_drv);
@@ -107,7 +112,8 @@ void guiTask(void *pvParameter) {
     indev_drv.type = LV_INDEV_TYPE_POINTER;
     lv_indev_drv_register(&indev_drv);
 #endif
-
+    
+    /* Create and start a periodic timer interrupt to call lv_tick_inc */
     const esp_timer_create_args_t periodic_timer_args = {
         .callback = &lv_tick_task,
         .name = "periodic_gui"
@@ -116,6 +122,27 @@ void guiTask(void *pvParameter) {
     ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
     ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
 
+    /* Create the demo application */
+    create_demo_application();
+    
+    while (1) {
+        vTaskDelay(1);
+
+        /* Try to take the semaphore, call lvgl related function on success */
+        if (pdTRUE == xSemaphoreTake(xGuiSemaphore, (TickType_t) LV_TICK_PERIOD_MS)) {
+            lv_task_handler();
+            xSemaphoreGive(xGuiSemaphore);
+       }
+    }
+
+    /* A task should NEVER return */
+    vTaskDelete(NULL);
+}
+
+static void create_demo_application(void)
+{
+    /* When using a monochrome display we only show "Hello World" centered on the 
+     * screen */
 #if defined CONFIG_LVGL_TFT_DISPLAY_MONOCHROME || \
     defined CONFIG_LVGL_TFT_DISPLAY_CONTROLLER_ST7735S
     
@@ -134,18 +161,13 @@ void guiTask(void *pvParameter) {
      * 0, 0 at the end means an x, y offset after alignment*/
     lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, 0);
 #else
+    /* TODO: Otherwise we show the selected demo */
     lv_demo_widgets();
 #endif
-    
-    while (1) {
-        vTaskDelay(1);
-        //Try to lock the semaphore, if success, call lvgl stuff
-        if (xSemaphoreTake(xGuiSemaphore, (TickType_t)10) == pdTRUE) {
-            lv_task_handler();
-            xSemaphoreGive(xGuiSemaphore);
-        }
-    }
+}
 
-    //A task should NEVER return
-    vTaskDelete(NULL);
+static void lv_tick_task(void *arg) {
+    (void) arg;
+
+    lv_tick_inc(LV_TICK_PERIOD_MS);
 }
