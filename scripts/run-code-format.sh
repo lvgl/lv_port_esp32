f068188620048ad88b12e7330808a3996250d320
C47D <carlos.santiago.diaz@gmail.com>
2020-08-21 08:42:01 -0500

    astyle: Add format configuration and script

---
+++ scripts/run-code-format.sh
@@ -0,0 +1,1 @@
+astyle --options=code-format.cfg "../main/main.c,../components/lvgl_esp32_drivers/*.c,*.h,../components/lvgl_esp32_drivers/lvgl_tft/*.c,*.h,../components/lvgl_esp32_drivers/lvgl_touch/*c,*.h"
