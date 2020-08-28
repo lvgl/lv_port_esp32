f0c395ab342c717f0a210adad20f5d6eb1614417
Jackson Ming Hu <huming2207@gmail.com>
2020-08-28 18:12:39 +1000

    jd79653a: create code file

---
+++ components/lvgl_esp32_drivers/lvgl_tft/jd79653a.c
@@ -0,0 +1,31 @@
+/**
+@file jd79653a.c
+@brief   GoodDisplay GDEW0154M09 e-paper display w/ FitiPower JD79653A
+@version 1.0
+@date    2020-08-28
+@author  Jackson Ming Hu <huming2207@gmail.com>
+
+
+@section LICENSE
+
+MIT License
+
+Copyright (c) 2020 Jackson Ming Hu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+
+#include "jd79653a.h"
+
+
---
+++ components/lvgl_esp32_drivers/lvgl_tft/jd79653a.h
@@ -0,0 +1,20 @@
+/**
+ * @file il3820.h
+ *
+ */
+
+#ifndef JD79653A_H
+#define JD79653A_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif // JD79653A_H
