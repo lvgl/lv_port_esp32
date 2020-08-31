1077499b3ee7d4024b8570e9c4448aa846582595
Jackson Ming Hu <huming2207@gmail.com>
2020-08-31 20:04:43 +1000

    jd79653a: fix some weird timing issues

--- components/lvgl_esp32_drivers/lvgl_tft/jd79653a.c
+++ components/lvgl_esp32_drivers/lvgl_tft/jd79653a.c
@@ -135,8 +135,13 @@ static const jd79653a_seq_t init_seq[] = {
         {0x60, {0x00}, 1},                             // TCON
         {0x50, {0x97}, 1},                             // VCOM sequence
         {0xe3, {0x00}, 1},                             // Power saving settings
-        {0x04, {},            0},                             // Power ON
 };
+
+static const jd79653a_seq_t power_on_seq[] = {
+        {0x50, {0x97}, 1}, // VCOM sequence
+        {0x04, {},     0}, // Power on
+};
+
 
 static const jd79653a_seq_t power_off_seq[] = {
         {0x50, {0xf7}, 1}, // VCOM sequence
@@ -200,6 +205,20 @@ static esp_err_t jd79653a_wait_busy(uint32_t timeout_ms)
     return ((bits & EVT_BUSY) != 0) ? ESP_OK : ESP_ERR_TIMEOUT;
 }
 
+static void jd79653a_power_on()
+{
+    jd79653a_spi_send_seq(power_on_seq, 2);
+    vTaskDelay(pdMS_TO_TICKS(10));
+    jd79653a_wait_busy(0);
+}
+
+static void jd79653a_power_off()
+{
+    jd79653a_spi_send_seq(power_off_seq, 2);
+    vTaskDelay(pdMS_TO_TICKS(10));
+    jd79653a_wait_busy(0);
+}
+
 static void jd79653a_load_partial_lut()
 {
     jd79653a_spi_send_cmd(0x20); // LUT VCOM register
@@ -233,6 +252,10 @@ static void jd79653a_partial_in()
     jd79653a_spi_send_cmd(0x00);
     jd79653a_spi_send_data(pst_use_reg_lut, sizeof(pst_use_reg_lut));
 
+    uint8_t vcom = 0xb7;
+    jd79653a_spi_send_cmd(0x50);
+    jd79653a_spi_send_data(&vcom, 1);
+
     // Dump LUT in
     jd79653a_load_partial_lut();
 
@@ -255,12 +278,18 @@ static void jd79653a_partial_out()
     jd79653a_spi_send_cmd(0x00);
     jd79653a_spi_send_data(pst_use_otp_lut, sizeof(pst_use_otp_lut));
 
+    uint8_t vcom = 0x97;
+    jd79653a_spi_send_cmd(0x50);
+    jd79653a_spi_send_data(&vcom, 1);
+
     // Out from partial!
     jd79653a_spi_send_cmd(0x92);
 }
 
 static void jd79653a_update_partial(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_t *data)
 {
+    jd79653a_power_on();
+    jd79653a_partial_in();
     ESP_LOGD(TAG, "x1: 0x%x, x2: 0x%x, y1: 0x%x, y2: 0x%x", x1, x2, y1, y2);
 
     size_t len = ((x2 - x1 + 1) * (y2 - y1 + 1)) / 8;
@@ -272,14 +301,6 @@ static void jd79653a_update_partial(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t 
     jd79653a_spi_send_data(ptl_setting, sizeof(ptl_setting));
 
     uint8_t *data_ptr = data;
-
-    // Fill OLD data
-    uint8_t old_data[EPD_ROW_LEN];
-    memset(old_data, 0x00, sizeof(old_data));
-    jd79653a_spi_send_cmd(0x10);
-    for (size_t h_idx = 0; h_idx < EPD_HEIGHT; h_idx++) {
-        jd79653a_spi_send_data(old_data, EPD_ROW_LEN);
-    }
 
     jd79653a_spi_send_cmd(0x13);
     for (size_t h_idx = 0; h_idx < EPD_HEIGHT; h_idx++) {
@@ -294,12 +315,15 @@ static void jd79653a_update_partial(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t 
     jd79653a_wait_busy(0);
 
     ESP_LOGD(TAG, "Partial updated");
+    jd79653a_partial_out();
+    jd79653a_power_off();
 }
 
 void jd79653a_fb_set_full_color(uint8_t color)
 {
+    jd79653a_power_on();
     uint8_t old_data[EPD_ROW_LEN];
-    memset(old_data, ~color, EPD_ROW_LEN);
+    memset(old_data, ~(color), EPD_ROW_LEN);
 
     // Fill OLD data (maybe not necessary)
     jd79653a_spi_send_cmd(0x10);
@@ -318,18 +342,21 @@ void jd79653a_fb_set_full_color(uint8_t color)
     jd79653a_spi_send_cmd(0x12); // Issue refresh command
     vTaskDelay(pdMS_TO_TICKS(100));
     jd79653a_wait_busy(0);
+
+    jd79653a_power_off();
 }
 
 void jd79653a_fb_full_update(uint8_t *data, size_t len)
 {
+    jd79653a_power_on();
     ESP_LOGI(TAG, "Performing full update, len: %u", len);
 
     uint8_t *data_ptr = data;
 
-    // Fill OLD data
-    uint8_t old_data[EPD_ROW_LEN] = {0};
+    // Fill OLD data (maybe not necessary)
+    uint8_t old_data[EPD_ROW_LEN] = { 0 };
     jd79653a_spi_send_cmd(0x10);
-    for (size_t h_idx = 0; h_idx < EPD_HEIGHT; h_idx++) {
+    for (size_t idx = 0; idx < EPD_HEIGHT; idx++) {
         jd79653a_spi_send_data(old_data, EPD_ROW_LEN);
     }
 
@@ -346,6 +373,8 @@ void jd79653a_fb_full_update(uint8_t *data, size_t len)
     jd79653a_spi_send_cmd(0x12); // Issue refresh command
     vTaskDelay(pdMS_TO_TICKS(100));
     jd79653a_wait_busy(0);
+
+    jd79653a_power_off();
 }
 
 void jd79653a_lv_set_fb_cb(struct _disp_drv_t *disp_drv, uint8_t *buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
@@ -383,14 +412,9 @@ void jd79653a_lv_fb_flush(lv_disp_drv_t *drv, const lv_area_t *area, lv_color_t 
         ESP_LOGD(TAG, "Refreshing in FULL");
         jd79653a_fb_full_update(buf, ((EPD_HEIGHT * EPD_WIDTH) / 8));
         partial_counter = EPD_PARTIAL_CNT; // Reset partial counter here
-        jd79653a_partial_in();  // ...and go back to partial mode!
     } else {
         jd79653a_update_partial(area->x1, area->y1, area->x2, area->y2, buf);
         partial_counter -= 1;   // ...or otherwise, decrease 1
-
-        if (partial_counter == 0) {
-            jd79653a_partial_out(); // If partial counter is 0, exit partial mode anyway
-        }
     }
 
     lv_disp_flush_ready(drv);
